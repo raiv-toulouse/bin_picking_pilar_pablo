fabf61873ef6dcbc7478bdc35b0d89a1ad3af64e
raiv.toulouse@icam.fr <raiv.toulouse@icam.fr>
2021-06-15 08:32:12 +0200

    best prediction is now a service AND a publisher

--- ai_manager/src/ai_manager/best_prediction.py
+++ ai_manager/src/ai_manager/best_prediction.py
@@ -2,19 +2,35 @@
 
 
 import rospy
+import random
 from ai_manager.srv import GetBestPrediction, GetBestPredictionResponse
+from ai_manager.msg import Prediction
+from ai_manager.msg import ListOfPredictions
 
-def get_best_prediction(req):
-    predictions = rospy.get_param("/list_of_predictions")
-    rospy.loginfo("%s is %s", rospy.resolve_name('/list_of_predictions'), predictions)
-    rospy.set_param('list_of_predictions', [])
-    return GetBestPredictionResponse(predictions[0][1], predictions[0][2])
+class NodeBestPrediction:
+    def __init__(self):
+        self.predictions = []
+        msg_list_pred = ListOfPredictions()
+        rospy.init_node('best_prediction')
+        s = rospy.Service('best_prediction_service', GetBestPrediction, self.get_best_prediction)
+        pub = rospy.Publisher('predictions', ListOfPredictions, queue_size=10)
+        while not rospy.is_shutdown():
+            msg = Prediction()
+            msg.proba = random.random()
+            msg.x = random.randint(0, 640)
+            msg.y = random.randint(0, 480)
+            self.predictions.append(msg)
+            msg_list_pred.predictions = self.predictions
+            pub.publish(msg_list_pred)
+            rospy.sleep(0.1)
+
+    def get_best_prediction(self, req):
+        last = len(self.predictions)
+        return GetBestPredictionResponse(self.predictions[last-1])
 
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('best_prediction')
-        s = rospy.Service('best_prediction_service', GetBestPrediction, get_best_prediction)
-        rospy.spin()
+        n = NodeBestPrediction()
     except rospy.ROSInterruptException:
         pass
